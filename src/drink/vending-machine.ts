cf5a4957e1cbd4236a9dc24de7ca72004660972c
BcRikko <s.shin1106@gmail.com>
2018-07-06 07:58:14 +0900

    :recycle: 名前を省略しない

--- src/drink/vending-machine.ts
+++ src/drink/vending-machine.ts
@@ -24,43 +24,43 @@ export class VendingMachine {
 
   /**
    * ジュースを購入する
-   * @param i 投入金額(100円と500円のみ受け付ける)
+   * @param payment 投入金額(100円と500円のみ受け付ける)
    * @param kindOfDrink ジュースの種類
    * @return 指定したジュース.在庫不足、釣り銭不足で変えなかった場合はnullが返される
    */
-  buy (i: Coin, kindOfDrink: DrinkType): Drink {
+  buy (payment: Coin, kindOfDrink: DrinkType): Drink {
     // 100円と500円だけ受け取る
-    if ((i !== Coin.OneHundred) && (i !== Coin.FiveHundred)) {
-      this.charge.push(i)
+    if ((payment !== Coin.OneHundred) && (payment !== Coin.FiveHundred)) {
+      this.charge.push(payment)
       return null
     }
 
     if ((kindOfDrink === DrinkType.Coke) && (this.stockOfCoke.quantity === 0)) {
-      this.charge.push(i)
+      this.charge.push(payment)
       return null
     } else if ((kindOfDrink === DrinkType.DietCoke) && (this.stockOfDietCoke.quantity === 0)) {
-      this.charge.push(i)
+      this.charge.push(payment)
       return null
     } else if ((kindOfDrink === DrinkType.Tea) && (this.stockOfTea.quantity === 0)) {
-      this.charge.push(i)
+      this.charge.push(payment)
       return null
     }
 
     // 釣り銭不足
-    if (i === Coin.FiveHundred && this.numberOfCoin.filter(a => a === Coin.OneHundred).length < 4) {
-      this.charge.push(i)
+    if (payment === Coin.FiveHundred && this.numberOfCoin.filter(a => a === Coin.OneHundred).length < 4) {
+      this.charge.push(payment)
       return null
     }
 
-    if (i === Coin.OneHundred) {
+    if (payment === Coin.OneHundred) {
       // 100円玉を釣り銭に使える
-      this.numberOfCoin.push(i)
-    } else if (i === Coin.FiveHundred) {
+      this.numberOfCoin.push(payment)
+    } else if (payment === Coin.FiveHundred) {
       // 400円のお釣り
       // 100円玉を釣り銭に使える
-      this.numberOfCoin.push(i)
+      this.numberOfCoin.push(payment)
 
-      for (let j = 0; j < (i - Coin.OneHundred) / Coin.OneHundred; j++) {
+      for (let i = 0; i < (payment - Coin.OneHundred) / Coin.OneHundred; i++) {
         this.charge.push(Coin.OneHundred)
         const coinIndex = this.numberOfCoin.findIndex(a => a === Coin.OneHundred)
         this.numberOfCoin.splice(coinIndex, 1)
